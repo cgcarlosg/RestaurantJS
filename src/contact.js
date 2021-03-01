27e34e83fa6eaea81dbcfe8c6091833dfaca854e
carlos <cgcarlosg@hotmail.com>
2021-02-28 22:47:21 -0500

    add styles, css and fix links broken

--- src/contact.js
+++ src/contact.js
@@ -1,28 +1,28 @@
 function createContact() {
-    const contact = document.createElement("div");
-    contact.classList.add("contact");
-  
-    const phoneNumber = document.createElement("p");
-    phoneNumber.textContent = "Phone: 111 111 111 111";
-  
-    const address = document.createElement("p");
-    address.textContent = "Bogota, Andinos Mall";
-  
-    const restaurantLocation = document.createElement("img");
-    restaurantLocation.src = "images/restaurant-location.jpg";
-    restaurantLocation.alt = "Mozzafiato restaurant location";
-  
-    contact.appendChild(phoneNumber);
-    contact.appendChild(address);
-    contact.appendChild(restaurantLocation);
-  
-    return contact;
-  }
-  
-  function loadContact() {
-    const main = document.getElementById("main");
-    main.textContent = "";
-    main.appendChild(createContact());
-  }
-  
-  export default loadContact;
+  const contact = document.createElement("div");
+  contact.classList.add("contact");
+
+  const phoneNumber = document.createElement("p");
+  phoneNumber.textContent = "Phone: 123 456 789";
+
+  const address = document.createElement("p");
+  address.textContent = "Addres: Bogota Andinos Mall";
+
+  const restaurantLocation = document.createElement("img");
+  restaurantLocation.src = "images/restaurant-location.jpg";
+  restaurantLocation.alt = "Frijolada restaurant location";
+
+  contact.appendChild(phoneNumber);
+  contact.appendChild(address);
+  contact.appendChild(restaurantLocation);
+
+  return contact;
+}
+
+function loadContact() {
+  const main = document.getElementById("main");
+  main.textContent = "";
+  main.appendChild(createContact());
+}
+
+export default loadContact;
--- src/website.js
+++ src/website.js
@@ -3,105 +3,105 @@ import loadMenu from "./menu";
 import loadContact from "./contact";
 
 function createHeader() {
-    const header = document.createElement("header");
-    header.classList.add("header");
-  
-    const restaurantName = document.createElement("h1");
-    restaurantName.classList.add("restaurant-name");
-    restaurantName.textContent = "Frijolada";
-  
-    header.appendChild(restaurantName);
-    header.appendChild(createNav());
-  
-    return header;
-  }
-  
-  function createNav() {
-    const nav = document.createElement("nav");
-  
-    const homeButton = document.createElement("button");
-    homeButton.classList.add("button-nav");
-    homeButton.textContent = "Home";
-    homeButton.addEventListener("click", (e) => {
-      if (e.target.classList.contains("active")) return;
-      setActiveButton(homeButton);
-      loadHome();
-    });
-  
-    const menuButton = document.createElement("button");
-    menuButton.classList.add("button-nav");
-    menuButton.textContent = "Menu";
-    menuButton.addEventListener("click", (e) => {
-      if (e.target.classList.contains("active")) return;
-      setActiveButton(menuButton);
-      loadMenu();
-    });
-  
-    const contactButton = document.createElement("button");
-    contactButton.classList.add("button-nav");
-    contactButton.textContent = "Contact";
-    contactButton.addEventListener("click", (e) => {
-      if (e.target.classList.contains("active")) return;
-      setActiveButton(contactButton);
-      loadContact();
-    });
-  
-    nav.appendChild(homeButton);
-    nav.appendChild(menuButton);
-    nav.appendChild(contactButton);
-  
-    return nav;
-  }
-  
-  function setActiveButton(button) {
-    const buttons = document.querySelectorAll(".button-nav");
-  
-    buttons.forEach((button) => {
-      if (button !== this) {
-        button.classList.remove("active");
-      }
-    });
-  
-    button.classList.add("active");
-  }
-  
-  function createMain() {
-    const main = document.createElement("main");
-    main.classList.add("main");
-    main.setAttribute("id", "main");
-    return main;
-  }
-  
-  function createFooter() {
-    const footer = document.createElement("footer");
-    footer.classList.add("footer");
-  
-    const copyright = document.createElement("p");
-    copyright.textContent = "Copyright © 2021 Carlos Gutierrez";
-  
-    const githubLink = document.createElement("a");
-    githubLink.href = "https://github.com/cgcarlosg";
-  
-    const githubIcon = document.createElement("i");
-    githubIcon.classList.add("fab");
-    githubIcon.classList.add("fa-github");
-  
-    githubLink.appendChild(githubIcon);
-    footer.appendChild(copyright);
-    footer.appendChild(githubLink);
-  
-    return footer;
-  }
-  
-  function initializeWebsite() {
-    const content = document.getElementById("content");
-  
-    content.appendChild(createHeader());
-    content.appendChild(createMain());
-    content.appendChild(createFooter());
-  
-    setActiveButton(document.querySelector(".button-nav"));
+  const header = document.createElement("header");
+  header.classList.add("header");
+
+  const restaurantName = document.createElement("h1");
+  restaurantName.classList.add("restaurant-name");
+  restaurantName.textContent = "Frijolada";
+
+  header.appendChild(restaurantName);
+  header.appendChild(createNav());
+
+  return header;
+}
+
+function createNav() {
+  const nav = document.createElement("nav");
+
+  const homeButton = document.createElement("button");
+  homeButton.classList.add("button-nav");
+  homeButton.textContent = "Home";
+  homeButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(homeButton);
     loadHome();
-  }
-  
-  export default initializeWebsite;
+  });
+
+  const menuButton = document.createElement("button");
+  menuButton.classList.add("button-nav");
+  menuButton.textContent = "Menu";
+  menuButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(menuButton);
+    loadMenu();
+  });
+
+  const contactButton = document.createElement("button");
+  contactButton.classList.add("button-nav");
+  contactButton.textContent = "Contact";
+  contactButton.addEventListener("click", (e) => {
+    if (e.target.classList.contains("active")) return;
+    setActiveButton(contactButton);
+    loadContact();
+  });
+
+  nav.appendChild(homeButton);
+  nav.appendChild(menuButton);
+  nav.appendChild(contactButton);
+
+  return nav;
+}
+
+function setActiveButton(button) {
+  const buttons = document.querySelectorAll(".button-nav");
+
+  buttons.forEach((button) => {
+    if (button !== this) {
+      button.classList.remove("active");
+    }
+  });
+
+  button.classList.add("active");
+}
+
+function createMain() {
+  const main = document.createElement("main");
+  main.classList.add("main");
+  main.setAttribute("id", "main");
+  return main;
+}
+
+function createFooter() {
+  const footer = document.createElement("footer");
+  footer.classList.add("footer");
+
+  const copyright = document.createElement("p");
+  copyright.textContent = "Copyright © 2021 Carlos Gutierrez";
+
+  const githubLink = document.createElement("a");
+  githubLink.href = "https://github.com/cgcarlosg";
+
+  const githubIcon = document.createElement("i");
+  githubIcon.classList.add("fab");
+  githubIcon.classList.add("fa-github");
+
+  githubLink.appendChild(githubIcon);
+  footer.appendChild(copyright);
+  footer.appendChild(githubLink);
+
+  return footer;
+}
+
+function initializeWebsite() {
+  const content = document.getElementById("content");
+
+  content.appendChild(createHeader());
+  content.appendChild(createMain());
+  content.appendChild(createFooter());
+
+  setActiveButton(document.querySelector(".button-nav"));
+  loadHome();
+}
+
+export default initializeWebsite;
